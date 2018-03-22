01c7aec81feead437429281d8eaa5a84eafe9c6d
Mauricio Benjamín García Tec <mauriciogtec@gmail.com>
2018-03-22 00:11:46 -0500

    run docker

--- prox-tv/run_docker.ps1
+++ prox-tv/run_docker.ps1
@@ -1,1 +1,1 @@
-docker run --rm -it -v c:/:/data/ -p 8889:8889 prox-tv:latest /bin/bash -c "jupyter notebook --ip='*' --port=8889  --allow-root"
+docker run --rm -it -v c:/:/data/ -p 8889:8889 proxtv:latest /bin/bash -c "jupyter notebook --ip='*' --port=8889  --allow-root"
